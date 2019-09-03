28a99b57351544deef85fae59397c78c7b7c7439
ddingjo <ddingjo@naver.com>
2019-09-03 16:36:42 +0900

    DbContext 생성

---
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Student>().ToTable("Student");
+            mb.Entity<Course>().ToTable("Course");
+            mb.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+}
